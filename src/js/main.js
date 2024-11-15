b807a7eb76c5c0f3f33c1f4cca170d4d151b8404
TOMCHRISTER\tomch <tomsch01266@stud.noroff.no>
2024-11-15 20:36:55 +0100

    feat: finished login and logout functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,34 @@
 import '../styles/input.css';
+
+// login and logout functions
+function loggedIn() {
+    const accessToken = sessionStorage.getItem('accessToken');
+    return accessToken !== null;
+}
+
+function updateLogin() {
+    const loginAnchor = document.getElementById('loginAnchor');
+    if (loggedIn()) {
+        loginAnchor.textContent = 'Log out';
+        loginAnchor.href = 'index.html';
+    } else {
+        loginAnchor.textContent = 'Login';
+        loginAnchor.href = 'account/login.html';
+    }
+}
+updateLogin()
+
+function logOut() {
+    event.preventDefault();
+    sessionStorage.removeItem('accessToken');
+    window.location.href = 'index.html';
+}
+
+const logoutBtn = document.getElementById('loginAnchor');
+if (logoutBtn) {
+    logoutBtn.addEventListener('click', function (event) {
+        if (loggedIn()) {
+            logOut(event)
+        }
+    });
+}
