987ae8be3167783561a6351ed2d0ce06c7e3e34f
TOMCHRISTER\tomch <tomsch01266@stud.noroff.no>
2024-12-12 18:48:03 +0100

    Fix: debug

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,16 @@ export default defineConfig({
         port: 3000,
     },
     build: {
+        outDir: 'dist',
         rollupOptions: {
             input: {
-                main: resolve (__dirname, 'index.html'),
-                login: resolve (__dirname, 'account/auth.html'),
-                profile: resolve (__dirname, 'profile/index.html'),
-                create: resolve (__dirname, 'profile/create.html'),
-                listing: resolve (__dirname, 'listing/singleListing.html'),
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'account/auth.html'),
+                profile: resolve(__dirname, 'profile/index.html'),
+                create: resolve(__dirname, 'profile/create.html'),
+                listing: resolve(__dirname, 'listing/singleListing.html'),
             },
         },
     },
+
 });
